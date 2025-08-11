eb6b34a8f6053298ec6df0ca638dd3bd7a29c249
Alexander McDougall-Morrison <a.mcdougallmorrison@gmail.com>
2025-08-11 22:47:24 +0100

    Javascript One - exercise 3 complete

---
+++ JAVASCRIPT_ONE/part3_exercise.js
@@ -0,0 +1,5 @@
+var lbs = prompt('Lbs:')
+var kilograms_per_lb = lbs*0.453592
+alert(kilograms_per_lb + "kg")
+alert('Conversion completed!')
+//alert("There are " kilograms_per_lb + "kilograms in " + lbs + "lbs")
