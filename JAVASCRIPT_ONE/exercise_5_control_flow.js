ce39bf7f4bc6dacda824a43214d2376e16430b49
Alexander McDougall-Morrison <a.mcdougallmorrison@gmail.com>
2025-08-12 23:04:19 +0100

    javascript control flow - exercise 5

--- JAVASCRIPT_ONE/exercise_5_control_flow.js
+++ JAVASCRIPT_ONE/exercise_5_control_flow.js
@@ -8,3 +8,16 @@ if (temp>80) {
 } else {
     null
 };
+
+var ham = 0;
+var cheese = 0;
+
+if (ham >=10 && cheese >= 10) {
+    report = "strong sales of ham and cheese"
+} else if (ham === 0 && cheese == 0) {
+    report = 'no sales!'
+} else {
+    report = 'We had some sales of items'
+}
+
+console.log(report);
