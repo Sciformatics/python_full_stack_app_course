8df10249d4c783403bb6c32e376c88b0971be152
Alexander McDougall-Morrison <a.mcdougallmorrison@gmail.com>
2025-09-04 23:40:46 +0100

    while loops even numbers

--- JAVASCRIPT_ONE/while_loops.js
+++ JAVASCRIPT_ONE/while_loops.js
@@ -4,5 +4,9 @@ while (x <5) {
     console.log("x is equal to " + x)
     console.log('This is less than 5, adding 1 to x:')
     console.log('Adding 1 to x:')
+    if (x===3) {
+        console.log('STOP!')
+        break
+    }
     x = x + 1;
 }
