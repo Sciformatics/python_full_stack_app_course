515394124ff2911b68c5621b24a0845740e5c15e
Alexander McDougall-Morrison <a.mcdougallmorrison@gmail.com>
2025-09-08 23:49:24 +0100

    basic for loops in js

---
+++ JAVASCRIPT_ONE/for_loops.js
@@ -0,0 +1,14 @@
+for (var i = 0; i < 60; i++) {
+    console.log("Number is "+i);
+}
+
+var word = "ABCDEFG"
+
+for (var i = 0; i < word.length; i++) {
+    console.log(word[i])
+}
+
+
+for (var i = 0; i < word.length; i+=2) {
+    console.log(word[i])
+}
