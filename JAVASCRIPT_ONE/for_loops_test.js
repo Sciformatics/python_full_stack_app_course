df2643f1734f7bfdb75699a86dbfd3995562aac6
Alexander McDougall-Morrison <a.mcdougallmorrison@gmail.com>
2025-09-09 23:35:03 +0100

    javascript for loops and while loops exercises

---
+++ JAVASCRIPT_ONE/for_loops_test.js
@@ -0,0 +1,18 @@
+// repeat word hello 5times using while and for loops in js
+
+
+// while loop
+
+var count = 0
+
+while (count < 5) {
+    console.log('hello - while loop')
+    count++
+}
+
+
+// for loop
+
+for (var i = 0; i < 5; i++) {
+    console.log('hello - for loop')
+}
