1b60b64bd29b28c365c8e73c8a04562ab648c612
Alexander McDougall-Morrison <a.mcdougallmorrison@gmail.com>
2025-09-14 18:45:05 +0100

    corrected remove() function

---
+++ JAVASCRIPT_TWO/arrays.js
@@ -0,0 +1,9 @@
+var testArray = ['science', 'python', 'exercsie']
+
+// testArray.forEach(console.log); // in-line for loop of elements - similar to list comprehensions in python
+
+// function print(testArray) { // verbose for loop 
+//     for (element of testArray) {
+//         console.log(element);
+//     }
+// };
