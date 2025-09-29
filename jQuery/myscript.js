aa59de9474dee0da6dc38f465fb59bab0551bad3
Alexander McDougall-Morrison <a.mcdougallmorrison@gmail.com>
2025-09-29 22:58:58 +0100

    used jquery to change the status of different tags using $, including .on, .keypress, .toggleClass and effects methds

--- jQuery/myscript.js
+++ jQuery/myscript.js
@@ -2,6 +2,47 @@
 //     console.log('There was a click')
 // })
 
-$('h1').click(function() {
-    $(this).text("I was changed")
+// $('h1').click(function() {
+//     $(this).text("I was changed")
+// })
+
+// $('input').eq(0).keypress(function() {
+//     $('h3').toggleClass('turnBlue');
+// })
+
+//keypress
+
+// $('input').eq(0).keypress(function(event) {
+//     console.log(event);
+// })
+
+// $('input').eq(0).keypress(function(event) {
+//     if (event.which === 13) {
+//         $('h3').toggleClass('turnBlue'); //which shows us the number of the key being pressed, e.g. a == 97
+//     }
+// })
+
+// on method
+
+// $('h1').on('dblclick', function() {
+//     $(this).toggleClass('turnBlue');
+// })
+
+// $('h1').on('mouseenter', function() {
+//     $(this).toggleClass('turnBlue');
+// })
+
+$('h1').on('mouseover', function() {
+    $(this).toggleClass('turnBlue');
+})
+
+
+//animations and effects
+
+// $('input').eq(1).on('click', function() {
+//     $('.container').fadeOut(3000) //in milliseconds
+// })
+
+$('input').eq(1).on('click', function() {
+    $('.container').slideUp(3000) //in milliseconds
 })
